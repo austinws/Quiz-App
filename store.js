8e0390782d0674218df62d928ceb1d6a11022fdc
austinws <60244004+austinws@users.noreply.github.com>
2020-04-02 21:21:25 -0700

    Create store.js

---
+++ store.js
@@ -0,0 +1,94 @@
+const STORE = {
+    questions: [
+        {
+            question: 'What is the highest mountain in Antarctica?',
+            options: [
+                "Vinson Massif",
+                "Sentinel Ridge",
+                "Penguin Summit",
+                "Mount Shinn"
+            ],
+            answer: "Vinson Massif",
+        },
+        //2
+        {
+            question: "What is the highest peak in North America?", 
+            options: [
+                "Mount Whitney",
+                "Denali",
+                "Mount Logan",
+                "King Peak"
+            ],
+            answer: "Denali",
+        },
+        //3
+        {
+            question: "What is the highest pile of rocks in South America?",
+            options: [
+                "Mercedario",
+                "Mount Chile",
+                "Ojos Del Salado",
+                "Aconcagua"
+            ],
+            answer: "Aconcagua",
+        },
+        //4
+        {
+            question: "What is the highest mountain in Australia?",
+            options: [
+                "Mount Bogong",
+                "Mount Kosciuszko",
+                "Mount Townsend",
+                "Gungartan"
+            ],
+            answer: "Mount Kosciuszko",
+        },
+        //5
+        {
+            question: "What is Europes highest peak?",
+            options: [
+                "Mount Elbrus",
+                "Mount Everest",
+                "Mount Perisher",
+                "Mount Alemans"
+            ],
+            answer: "Mount Elbrus",
+        },
+        //6
+        {
+          question: "In Asia, what is the tallest mountain?",
+          options: [
+            "The Ghengis Range",
+            "Mount Everest",
+            "K2",
+            "Kangchenjunga"
+          ],
+          answer: "Mount Everest",
+        },
+        //7
+        {
+          question: "What is the tallest mountain on planet Earth?",
+          options:[
+            "Mount Everest",
+            "Mount Fuji",
+            "Really Tall Mountain",
+            "The island of Mauna Kea"
+          ],
+          answer: "The island of Mauna Kea",
+        },
+        //8
+        {
+          question: "In this solar system, which mountain is the highest?",
+          options:[
+            "Tenzing Montes, Pluto",
+            "Olympus Mons, Mars",
+            "Skadi Mons, Venus",
+            "Mount Everest, Earth"
+
+          ],
+          answer: "Olympus Mons, Mars",
+        }
+    ],
+    currentQuestion: 0,
+    score: 0,
+};
